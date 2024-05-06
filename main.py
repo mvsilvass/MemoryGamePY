106b29f2cd5180c2c153a8c98e2dc140c0360094
mvsilvass <mvsilvasoaress@gmail.com>
2024-05-06 12:00:52 -0300

    Adiciona a funçao generate_board no arquivo main.py

--- main.py
+++ main.py
@@ -16,10 +16,18 @@ class Game():
         
         #pygame.display.set_icon(self.icon)
         self.clock = pygame.time.Clock()
+        self.new_board = True
+        self.board_state = []
+        self.tamanho = []
         
     def run(self):
         while True:
             self.clock.tick(FPS)
+            
+            if self.new_board:
+                self.board_state = self.generate_board()
+                self.new_board = False
+                
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -34,10 +42,29 @@ class Game():
 
         for coluna in coordenadas:
             for coordenada in coluna:
+                self.tamanho.append(coordenada)
                 self.display_surface.blit(self.back_carta, coordenada)
 
         pygame.display.update()
         
+    def generate_board(self):
+        options_list = []
+        spaces_list = []
+        used_list = []
+    
+        for item in range(30// 2):
+            options_list.append(item)
+        
+        for item in range(30):
+            piece = options_list[random.randint(0, len(options_list)-1)]
+            spaces_list.append(piece)
+            
+            if piece in used_list:
+                used_list.remove(piece)
+                options_list.remove(piece)
+            else:
+                used_list.append(piece)
+                
 if __name__== "__main__":
     game = Game()
     game.run()

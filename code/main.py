4bd0b66350d4ddedf023d62f64e969bf273b2bc2
mvsilvass <mvsilvasoaress@gmail.com>
2024-05-21 15:56:57 -0300

    Atualiza a cor para Steelblue

--- code/main.py
+++ code/main.py
@@ -44,7 +44,7 @@ class Game():
                 self.check_matching_cards()
             
     def draw_window(self):
-        self.display_surface.fill(GRAY)     
+        self.display_surface.fill(STEELBLUE)     
 
         for coluna in COORDENADAS_IMAGE.values():
             for coordenada in coluna:
